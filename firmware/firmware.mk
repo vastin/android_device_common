fa359831c2ff2b848c5ba2be429ffa5cf88cbc75
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-08-09 15:10:19 +0800

    change firmware rules to be compatible with honeycomb build system

--- firmware/firmware.mk
+++ firmware/firmware.mk
@@ -12,4 +12,17 @@ LOCAL_FIRMWARES_DIR ?= $(call my-dir)
 LOCAL_PATH := $(LOCAL_FIRMWARES_DIR)
 LOCAL_FIRMWARES ?= $(filter-out firmware.mk,$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH) -type f)))
 
-PRODUCT_COPY_FILES += $(foreach f,$(LOCAL_FIRMWARES),$(LOCAL_PATH)/$(f):system/lib/firmware/$(f))
+define include-firmware
+LOCAL_PATH := $(1)
+include $$(CLEAR_VARS)
+LOCAL_MODULE := $(2)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FIRMWARE
+LOCAL_MODULE_PATH := $$(TARGET_OUT)/lib/firmware
+LOCAL_SRC_FILES := $(2)
+include $$(BUILD_PREBUILT)
+
+ALL_DEFAULT_INSTALLED_MODULES += $$(LOCAL_INSTALLED_MODULE)
+endef
+
+$(foreach f,$(LOCAL_FIRMWARES),$(eval $(call include-firmware,$(LOCAL_PATH),$(f))))
